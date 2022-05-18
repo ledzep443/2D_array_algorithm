78a6a94e647c6fcef9b3028b1e0dbe411889e308
Jimmy Purnell <jimmy@purnellsoftwaredevelopment.com>
2022-05-17 20:12:34 -0400

    Fixed formatting

--- 2darray.js
+++ 2darray.js
@@ -2,25 +2,25 @@ let target_weight;
 let brownie_trays;
 function filterWeight(target_weight, brownie_trays) {
     if(target_weight > 0) {
-    var good_brownies = [];
-    brownie_trays.forEach(brownie_tray => {
-        //console.log(brownie_tray);
-        let good_tray = [];
-        for(let i = 0; i < brownie_tray.length; i++) {
-            
-            if(brownie_tray[i] >= target_weight) {
-                good_tray.push(brownie_tray[i]);
+        var good_brownies = [];
+        brownie_trays.forEach(brownie_tray => {
+            //console.log(brownie_tray);
+            let good_tray = [];
+            for(let i = 0; i < brownie_tray.length; i++) {
                 
-                
+                if(brownie_tray[i] >= target_weight) {
+                    good_tray.push(brownie_tray[i]);
+                    
+                    
+                }
             }
-        }
-        //console.log(good_tray);
-        good_brownies.push(good_tray);
-});
-    return good_brownies;
-} else {
-    return "Please enter a positive number for the target weight";
-}
+            //console.log(good_tray);
+            good_brownies.push(good_tray);
+        });
+        return good_brownies;
+    } else {
+        return "Please enter a positive number for the target weight";
+    }
 }
 
 console.log(filterWeight(2, [[3, 5, 1], [3, 6, 8], [1, 2, 5]]));
